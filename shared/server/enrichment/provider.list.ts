892d165fd050e96f65eb9ccfc0e582a55ea958b1
Nevo David <me@nevos.io>
2025-09-04 19:29:48 +0700

    feat: hunter.io

--- shared/server/enrichment/provider.list.ts
+++ shared/server/enrichment/provider.list.ts
@@ -2,9 +2,11 @@ import { ApolloEnrichment } from '@growchief/shared-backend/enrichment/providers
 import { DatagmaEnrichment } from '@growchief/shared-backend/enrichment/providers/datagma.enrichment';
 import { EnrichmentInterface } from '@growchief/shared-backend/enrichment/enrichment.interface';
 import { RocketReachEnrichment } from '@growchief/shared-backend/enrichment/providers/rocket.reach.enrichment';
+import { HunterEnrichment } from '@growchief/shared-backend/enrichment/providers/hunter.enrichment';
 
 export const providerList = [
-  new RocketReachEnrichment(),
-  new ApolloEnrichment(),
-  new DatagmaEnrichment(),
+  // new RocketReachEnrichment(),
+  // new ApolloEnrichment(),
+  // new DatagmaEnrichment(),
+  new HunterEnrichment(),
 ].sort((a, b) => a.priority - b.priority) as EnrichmentInterface[];
---
+++ shared/server/enrichment/providers/hunter.enrichment.ts
@@ -0,0 +1,65 @@
+import {
+  EnrichmentInterface,
+  EnrichmentReturn,
+} from '@growchief/shared-backend/enrichment/enrichment.interface';
+import { EnrichmentDto } from '@growchief/shared-both/dto/enrichment/enrichment.dto';
+
+export class HunterEnrichment implements EnrichmentInterface {
+  name = 'Hunter';
+  priority = 1;
+  supportedIdentifiers = ['linkedin'];
+
+  get apiKey() {
+    return process.env.HUNTER_API_KEY;
+  }
+
+  async enrich(
+    platform: string,
+    { email }: EnrichmentDto,
+  ): Promise<EnrichmentReturn | false | { delay: number }> {
+    const listQuery = {
+      ...(email ? { email } : {}),
+    };
+
+    if (Object.keys(listQuery).length === 0) {
+      return false;
+    }
+
+    const list = new URLSearchParams(listQuery).toString();
+    const req = await fetch(
+      `https://api.hunter.io/v2/people/find?${list}&api_key=${process.env.HUNTER_API_KEY}`,
+      {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          'Cache-Control': 'no-cache',
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+
+    if (req.status === 403) {
+      return { delay: 2000 };
+    }
+
+    const value = await req.json();
+
+    console.log(value);
+    if (
+      !value?.data?.id ||
+      !value?.data?.linkedin?.handle ||
+      platform !== 'linkedin'
+    ) {
+      return false;
+    }
+
+    const [firstName, ...lastName] = value?.data?.name?.fullName || '';
+
+    return {
+      firstName: firstName || '',
+      lastName: lastName.join(' ') || '',
+      url: 'https://www.linkedin.com/in/' + value?.data?.linkedin?.handle,
+      picture: '',
+    };
+  }
+}
